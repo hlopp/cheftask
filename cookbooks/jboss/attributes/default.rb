6f305b741758cc3f9ff738f2b3664748543f92d3
root <dzinis_manulik@epam.com>
2015-06-04 05:16:08 -0400

    mod_jk works correctly

--- cookbooks/jboss/attributes/default.rb
+++ cookbooks/jboss/attributes/default.rb
@@ -1,4 +1,5 @@
 default['jboss']['jboss_home'] = "/opt/jboss" 
-default['jboss']['url'] = 
-"http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.zip" 
+default['jboss']['url'] = "http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.zip" 
 default['jboss']['name'] = "jboss-as-7.1.1.Final"
+default['httpd']['mod_jk']['url'] = "http://www.eu.apache.org/dist/tomcat/tomcat-connectors/jk/tomcat-connectors-1.2.40-src.tar.gz"
+default['httpd']['mod_jk']['name'] = "tomcat-connectors-1.2.40-src"
--- cookbooks/jboss/recipes/default.rb
+++ cookbooks/jboss/recipes/default.rb
@@ -11,7 +11,8 @@
 jboss_home = node['jboss']['jboss_home'] 
 jboss_url = node['jboss']['url']
 jboss_name = node['jboss']['name']
-
+modjk_url = node['httpd']['mod_jk']['url']
+modjk_name = node['httpd']['mod_jk']['name']
 
 #Installing unzip
 yum_package 'unzip' do
@@ -26,6 +27,12 @@ end
 execute "add_httpd_chkconfig" do
 	command "chkconfig httpd on"
 end
+
+
+yum_package 'httpd-devel' do
+        action :install
+end
+
 
 cookbook_file "jboss.conf" do
 	path "/etc/httpd/conf.d/jboss.conf"
@@ -67,6 +74,7 @@ execute "unzip_jboss" do
 end
 
 
+#Creating init script for jboss
 cookbook_file "jboss" do
 	path "/etc/init.d/jboss"
 	mode "0755"
@@ -77,4 +85,59 @@ end
 
 service "jboss" do
     action :start 
+end
+
+#Downloading mod_jk for apache
+remote_file "#{modjk_name}.tar.gz" do
+	source modjk_url
+	path "/tmp/#{modjk_name}.tar.gz"
+	action :create_if_missing 
+end 
+
+execute "extract mod_jk" do
+  command "tar -xvf /tmp/#{modjk_name}.tar.gz"
+end
+
+
+bash "modjk_manipulations" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+    tar -xvf #{modjk_name}.tar.gz
+    yum install gcc make automake libtool
+    cd tomcat-connectors-1.2.40-src/native
+    ./buildconf.sh
+    ./configure --with-apxs=/usr/sbin/apxs
+    make
+    make install
+  EOH
+end
+
+
+cookbook_file "modjk.conf" do
+        path "/etc/httpd/conf.d/modjk.conf"
+        mode "0755"
+        group "root"
+        owner "root"
+        action :create_if_missing
+end
+
+cookbook_file "workers.properties" do
+	path "/etc/httpd/conf/workers.properties"
+	mode "0755"
+	group "root"
+	owner "root"
+	action :create_if_missing
+end
+
+cookbook_file "httpd.conf" do
+        path "/etc/httpd/conf/httpd.conf"
+        mode "0755"
+        group "root"
+        owner "root"
+        action :create_if_missing
+end
+
+service "httpd" do
+  action :restart
 end
