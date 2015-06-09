a677cbe7fb8a1c0544251a20ee1bd47192e75ce7
root <dzinis_manulik@epam.com>
2015-06-09 08:14:39 -0400

    First commit

---
+++ cookbooks/book/attributes/default.rb
@@ -0,0 +1,25 @@
+default[:book][:packages] = [
+    'epel-release', 
+    'lsof', 
+    'htop', 
+    'vim-enhanced.x86_64',
+]
+
+default[:book][:users] = [
+    {
+      'username' => 'Jack',
+      'password' => '1234',
+      'uid' => 601,
+      'gid' => 'user',
+      'home_dir' => '/home/Jack/',
+      'def_shell' => '/bin/bash/',
+    },
+    {
+      'username' => 'John',
+      'password' => '123456',
+      'uid' => 602,
+      'gid' => 'user',
+      'home_dir' => '/home/John/',
+      'def_shell' => '/bin/bash/',
+    },
+]
---
+++ cookbooks/book/recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: book
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+#Installing required packages
+packages = node[:book][:packages]
+packages.each do |pck|
+    yum_package pck do
+        action:install
+    end
+end
+
+#Creating users
+passwd_templ = node[:book][:users]
+passwd_templ.each do |new_user|
+    user new_user[:username] do
+        action :create
+        password new_user[:password]
+        uid new_user[:uid]
+        gid new_user[:gid]
+        home new_user[:home_dir]
+        shell new_user[:def_shef]
+    end
+end        
+        
